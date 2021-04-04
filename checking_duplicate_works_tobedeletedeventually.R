fd0e540f09b333b8d35d799a881530861aa10d68
jjmedinaariza <juanjo.medinaariza@googlemail.com>
2021-04-04 16:46:21 +0200

    Added ppp object with marks. Nextstep is to include KDE with marks.

--- checking_duplicate_works_tobedeletedeventually.R
+++ checking_duplicate_works_tobedeletedeventually.R
@@ -8,3 +8,10 @@ print(um)
 Y <- ppp(c(1,1,0.5,1,3,3,3,3,3,3,1), c(2,2,1,2,5,5,5,5,5,5,4),
          window=square(6), check=FALSE, marks = m)
 plot(Y)
+
+
+vals <- values(r1)
+hist(vals)
+vals <- (vals*1000)/0.3048006096
+hist(vals)
+r1 <-setValues(r1, vals) #change values in r1 by those we stored in vals
