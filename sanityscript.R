46d4ce3722d81a72db1099d85838ea7460d30b50
jjmedinaariza <juanjo.medinaariza@googlemail.com>
2021-04-07 17:34:19 +0200

    Changes to Chapter 6, which will probably for the most part not survive the cleaning tomorrow

--- sanityscript.R
+++ sanityscript.R
@@ -30,3 +30,83 @@ st_write(manchester, "data/manchester", driver = "GeoJSON")
 ?st_write
 z <- st_read("data/manchester")
 hist(manchester$crimr2)
+
+
+We do indeed, as we made sure in the previous section.
+Now we can move on to our spatial operation, where we select
+only those points within the city centre polygon. To do this,
+we first make a list of intersecting polints to the polygon,
+useing the `st_intersects()` function. This function takes
+two arguments, first the polygon which we want to subset our
+points within, and second, the points which we want to subset.
+We then use the resulting `cc_crimes` object to subset the crimes
+object to include only those which intersect (return `TRUE` for
+                                              intersects):
+
+  ```{r}
+# intersection
+manhattan_housing <- st_intersects(ctshp, nyc_houses)
+# subsetting
+manhattan_housing <- nyc_houses[unlist(manhattan_housing),]
+plot(manhattan_housing)
+plot(ctshp)
+
+library(tidycensus)
+census_api_key("c466e96ee65ca63b7de478ba06e94dc381c6a14d")
+census_var <- load_variables(2010, "sf1", cache = TRUE)
+View(census_var)
+hous_units <- get_decennial(geography = "tract",
+                            variables = "H001001",
+                            year = 2010,
+                            state = "New York",
+                            county = "New York County",
+                            geometry = TRUE,
+                            keep_geo_vars = TRUE)
+tm_shape(hous_units) +
+  tm_polygons("value")
+shp_name <- "data/NYC_Census_Tracts_for_2010_US_Census/NYC_Census_Tracts_for_2010_US_Census.shp"
+ct <- st_read(shp_name)
+tm_shape(ct) +
+  tm_polygons("BoroCode")
+ct_manhattan <- filter(ct, BoroCode==1)
+tm_shape(ct_manhattan) +
+  tm_polygons("BoroCode")
+rm(ct)
+summary(as.numeric(ct_manhattan$CT2010))
+summary(as.numeric(hous_units$TRACT))
+hous_units <- dplyr::select(hous_units, TRACT, value)
+st_geometry(hous_units) <- NULL
+hous_units <- rename(hous_units, CT2010 = TRACT)
+ct <- merge(ct_manhattan, hous_units, by = "CT2010")
+tm_shape(ct) +
+  tm_polygons("value")
+st_write(ct, "data/nyc/manhattan_housing.shp")
+
+
+st_write(hous_units, "data/nyc/manhattan_housing.shp")
+shp_name <- "data/nyc/manhattan_housing.shp"
+housing <- st_read(shp_name)
+housing <- st_transform(housing, 32118)
+shp_name <- "data/NYC_Census_Tracts_for_2010_US_Census/NYC_Census_Tracts_for_2010_US_Census.shp"
+ct <- st_read(shp_name)
+ct <- st_transform(housing, 32118)
+ok <- dplyr::select(housing, GEO_ID, value)
+st_geometry(ok) <- NULL
+please <- merge(ct,ok,by="GEO_ID")
+tm_shape(please) +
+  tm_polygons("value.x")
+st_write(hous_units, "data/nyc/manhattan_housing.shp")
+
+tm_shape(manhattan_housing) +
+  tm_polygons("totalunits")
+
+
+We do indeed, as we made sure in the previous section.  Now we can move on to our spatial operation, where we select only those points within the city centre polygon. To do this, we first make a list of intersecting polints to the polygon, useing the `st_intersects()` function. This function takes two arguments, first the polygon which we want to subset our points within, and second, the points which we want to subset. We then use the resulting `cc_crimes` object to subset the crimes object to include only those which intersect (return `TRUE` for intersects):
+
+  ```{r}
+# intersection
+cc_crimes <- st_intersects(city_centre, crimes)
+# subsetting
+cc_crimes <- crimes[unlist(cc_crimes),]
+
+
